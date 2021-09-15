cb030bf36afcbbe955c8ffef7faac21189100c40
EvVa <EvVa>
2021-09-15 17:26:03 +0300

    321

--- db/install.sh
+++ db/install.sh
@@ -5,6 +5,7 @@ echo 'instaling from CUSTOM_REPO_PATH/db'
 DB_URI_QUOTED=`python3 -c "import urllib.parse; parsed = urllib.parse.urlparse('$DB_URI'); print('$DB_URI'.replace(parsed.password, urllib.parse.quote(parsed.password)))"`
 if [[ ! -z $CUSTOM_REPO_PATH ]]; then
   pushd $CUSTOM_REPO_PATH/db
+  chmod u+x install.sh
   ./install.sh
   popd
 fi
